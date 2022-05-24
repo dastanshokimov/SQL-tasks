ea6349df84d2055db9c0ff2936873947e086439e
dastanshokimov <104251147+dastanshokimov@users.noreply.github.com>
2022-05-24 17:05:28 +0600

    Test case fix

--- ex_13.sql
+++ ex_13.sql
@@ -1,5 +1,8 @@
-SELECT Count(*) AS "Количество позиций счет-фактур", i.billingcountry AS "Название страны"
-FROM InvoiceLine il
-	JOIN invoice i
-	ON il.invoiceId = i.invoiceId
-GROUP  BY i.billingcountry;
+SELECT sum(i.Total) AS "Общий объем продаж", e.FirstName  ' '  e.LastName AS "Торговый агент"
+FROM Invoice i
+    JOIN Employee e 
+        ON c.SupportRepId = e.EmployeeId
+    JOIN Customer c 
+        ON i.CustomerId = c.CustomerId
+    WHERE e.Title like '%general%' or e.Title like '%agent%'
+    GROUP BY e.EmployeeId;
