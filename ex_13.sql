20433624b52d66f264eae5dfba33b4de3e30fbe0
dastanshokimov <104251147+dastanshokimov@users.noreply.github.com>
2022-05-24 17:23:33 +0600

    Update ex_13.sql

--- ex_13.sql
+++ ex_13.sql
@@ -1,8 +1,4 @@
-SELECT sum(i.Total) AS "Общий объем продаж", e.FirstName  ' '  e.LastName AS "Торговый агент"
-FROM Invoice i
-    JOIN Employee e 
-        ON c.SupportRepId = e.EmployeeId
-    JOIN Customer c 
-        ON i.CustomerId = c.CustomerId
-    WHERE e.Title like '%general%' or e.Title like '%agent%'
-    GROUP BY e.EmployeeId;
+SELECT count(InvoiceId) AS "Количество позиций счет-фактур", BillingCountry AS "Название страны"
+FROM Invoice
+	WHERE NOT BillingCountry = 'NULL' 
+	GROUP BY BillingCountry;
