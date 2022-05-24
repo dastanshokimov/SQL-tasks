eac7460546c46a0db6b9ac2c01f92ab8b3649c2d
dastanshokimov <104251147+dastanshokimov@users.noreply.github.com>
2022-05-24 16:53:20 +0600

    Test case fix

--- ex_11.sql
+++ ex_11.sql
@@ -1,4 +1,5 @@
 SELECT i.InvoiceId AS "Идентификатор счет-фактуры", t.Name AS "Название трека" 
 FROM InvoiceLine i
 	JOIN Track t 
-	ON i.TrackId = t.TrackId;
+	ON i.TrackId = t.TrackId
+	GROUP BY t.Name;
