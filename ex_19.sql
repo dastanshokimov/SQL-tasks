a305b92d88699431aa59b2f352ed6dddc374ddd2
dastanshokimov <104251147+dastanshokimov@users.noreply.github.com>
2022-05-25 12:43:10 +0600

    Update ex_19.sql

--- ex_19.sql
+++ ex_19.sql
@@ -1,8 +1,10 @@
-SELECT sum(i.Total) AS "Сумма продаж за 2009 год", e.FirstName || ' ' || e.LastName AS "Продавец"
+SELECT sum(i.Total) AS "Сумма продаж за 2010 год", e.FirstName || ' ' || e.LastName AS "Продавец"
 FROM Invoice i
 	JOIN Customer c 
 		ON i.CustomerId = c.CustomerId
 	JOIN Employee e 
-		ON c.CustomerId = e.EmployeeId
-	WHERE e.Title like '%agent%' AND i.InvoiceDate like '%2010%'
-	GROUP BY EmployeeId;
+		ON c.SupportRepId = e.EmployeeId
+	WHERE i.InvoiceDate like '%2010%'
+	GROUP BY c.SupportRepId
+	ORDER BY sum(i.Total) DESC
+	LIMIT 1;
